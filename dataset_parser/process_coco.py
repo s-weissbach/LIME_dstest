a0d657325af6b0ca58c6ef4b3bc3ec24749aaf8c
Stephan Weißbach <s.weissbach@uni-mainz.de>
2024-03-27 14:14:21 +0100

    rename function for better readability

---
+++ dataset_parser/process_coco.py
@@ -0,0 +1,13 @@
+from utils.dataloader import json_coco
+from utils.process import process_coco_annotation
+from utils.validate import validate_coco
+from utils.export import json_export
+
+import argparse
+
+
+def main(coco_path: str, export_path: str, ) -> None:
+    coco_json = 
+
+if __name__ == '__main__':
+    
