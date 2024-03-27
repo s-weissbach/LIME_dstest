2b5c473eab627b74eef018f30f4f4b88c171e9b3
Stephan Weißbach <s.weissbach@uni-mainz.de>
2024-03-27 13:33:17 +0100

    export function

---
+++ dataset_parser/export.py
@@ -0,0 +1,43 @@
+import os
+import json
+
+
+def json_export(annotation_dict: dict, path: str, overwrite: bool = False) -> None:
+    """
+    Export dictionary to a JSON file.
+
+    Parameters:
+        annotation_dict (dict): Dictionary to be exported to JSON.
+        path (str): Path to the JSON file.
+        overwrite (bool, optional): Whether to overwrite the file if it already exists.
+            Defaults to False.
+
+    Raises:
+        FileExistsError: If the file already exists and overwrite is False.
+        PermissionError: If the directory cannot be created due to lack of permissions.
+
+    Returns:
+        None
+    """
+    # ensure absolute path
+    path = os.path.abspath(path)
+    # ensure existence of directory
+    directory = os.path.dirname(path)
+    # Ensure existence of directory
+    directory = os.path.dirname(path)
+    try:
+        print(f"Directory ({directory}) did not exists and was created.")
+        os.makedirs(directory)
+    except FileExistsError:
+        # Directory already exists, no action needed
+        pass
+    except PermissionError:
+        raise PermissionError(f"Permission denied to create directory: {directory}")
+    # check if file exists
+    if os.path.exists(path) and not overwrite:
+        raise FileExistsError(
+            f"File {path} already exists. Use overwrite=True, to overwrite this file."
+        )
+    # dump file into json
+    with open(path, "w") as f:
+        json.dump(annotation_dict, f)
